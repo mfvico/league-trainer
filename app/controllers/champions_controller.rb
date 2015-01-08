35aa7d03358ab5175a9b765e068116d1917d3b7d
Mello <dan.mello1001@gmail.com>
2015-01-08 10:59:15 -0700

    cleaned up code base a bit

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -8,8 +8,24 @@ require 'open-uri'
 
   def show
     @champion = JSON.parse open("http://ddragon.leagueoflegends.com/cdn/4.20.1/data/en_US/champion/#{params[:id]}.json").read
-    @hp = @champion["data"]["#{params[:id]}"]["stats"]["hp"]
-    @hpregen = @champion["data"]["#{params[:id]}"]["stats"]["hpregen"]
+    @hp = stats["hp"]
+    @hpregen = stats["hpregen"]
+    @partype = @champion["data"]["#{params[:id]}"]["partype"]
+    @mp = stats["mp"]
+    @mpregen = stats["mpregen"]
+    @movespeed = stats["movespeed"]
+    @armor = stats["armor"]
+    @armorperlevel = stats["armorperlevel"]
+    @spellblock = stats["spellblock"]
+    @spellblockperlevel = stats["spellblockperlevel"]
+    @attack_damage = stats["attackdamage"]
+    @attack_damage_per_level = stats["attackdamageperlevel"]
+  end
+
+  private
+
+  def stats
+    @champion["data"]["#{params[:id]}"]["stats"]
   end
 
 
