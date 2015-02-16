434cc561bfba76f4f16075e6b2d72e671f10291e
Mello <dan.mello1001@gmail.com>
2015-02-16 15:08:35 -0700

    rendering individual summoner rune pages

--- app/controllers/runes_controller.rb
+++ app/controllers/runes_controller.rb
@@ -1,8 +1,24 @@
 class RunesController < ApplicationController
 
   def index
-    @rune_page = Summoner.new.summoner_runes(current_user.summoner_id)
+    @rune_pages = Summoner.new.summoner_runes(current_user.summoner_id)
   end
+
+  def show
+
+    raw_data = Summoner.new.summoner_runes(current_user.summoner_id)
+    raw_data.each do |id, pages|
+      pages[:pages].each do |page|
+        if page[:id] == params[:id].to_i
+          @rune_page = page
+        end
+      end
+    end
+  
+  end
+
+  private
+
 
 
 end
